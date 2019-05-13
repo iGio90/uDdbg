967bf9ecfa9377f86ea857fea4fa79e37aac0f5b
Dominik Maier <domenukk@gmail.com>
2019-05-13 07:08:54 +0200

    Added x86 fixes Added emulation mode and example

---
+++ examples/rtooos_emulator.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python3
+"""
+RTOoOS Emulator for Defcon Quals 2019
+
+This is an example using uDdbg embedded in "normal" unicorn code.
+
+To run, download the crux challenge binary here:
+https://github.com/o-o-overflow/dc2019q-rtooos/raw/master/crux/crux_7377a1f43e35924971ef1b172c080e03131bed56
+"""
+from __future__ import print_function
+import subprocess
+import sys
+from unicorn import *
+from unicorn.x86_const import *
+import capstone
+
+# We'll add the parent path to PYTHONPATH to be able to run this inside ./example
+import os.path
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+from udbg import UnicornDbg
+
+__author__ = "domenukk"
+
+# code to be emulated
+try:
+    with open("./crux_7377a1f43e35924971ef1b172c080e03131bed56", "rb") as bf:
+        GUEST_BINARY = bf.read()
+except:
+    print("Please drop a valid RTOoOS binary in this folder:")
+    print("wget https://github.com/o-o-overflow/dc2019q-rtooos/raw/master/crux/crux_7377a1f43e35924971ef1b172c080e03131bed56")
+    exit(1)
+
+# Not used for now.
+TXT_POS = 0x0
+TXT_SIZE = 8 * 1024 * 1024
+
+HEAP_POS = TXT_SIZE + 0x4096
+HEAP_SIZE = TXT_SIZE
+
+STACK_POS = 16 *  HEAP_POS + 0x4096
+
+HEAP_MIN = 0x1000
+HEAP_MAX = 0x3650
+
+COMPLETE_SIZE = 16 * 1024 * 1024
+
+#ENTRY = TXT_POS + 0x13f0
+ENTRY = TXT_POS # TXT_POS + 0x13f0
+
+def read_str(uc: Uc, pos: int) -> str:
+    ret = ""
+    char = chr(uc.mem_read(pos, 1)[0])
+    while char != '\0':
+        ret += char
+        pos += 1
+        char = chr(uc.mem_read(pos, 1)[0])
+    return ret
+
+
+# callback for OUT instruction
+def hook_out(uc: Uc, port, size, value, user_data):
+    try: 
+        eip = uc.reg_read(UC_X86_REG_EIP)
+        hyp_num = uc.reg_read(UC_X86_REG_EDI)
+        hypercall = chr(hyp_num)
+        #print("Got Hypercall {}".format(hypercall))
+
+        if hypercall == "a":
+            # putc
+            sys.stdout.write(chr(uc.reg_read(UC_X86_REG_RAX) & 0xFF))
+            sys.stdout.flush()
+
+        elif hypercall == "b":
+            # WE DONT KNOW
+            print("You triggered this Hypercall... We don't know what it does! No go do this on the real binary.")
+
+        elif hypercall == "c":
+            # read
+                # read
+            input = sys.stdin.buffer.readline()
+            input += b"\0"
+
+            uc.mem_write(uc.reg_read(UC_X86_REG_RAX), input)
+            uc.reg_write(UC_X86_REG_RAX, len(input) - 1) # 0 bytes?
+
+        elif hypercall == "d":
+            # puts
+            output = read_str(uc, uc.reg_read(UC_X86_REG_RAX))
+            print(output)
+
+        elif hypercall == "e":
+            # ls
+            print("Calling ls.")
+            ls_out = subprocess.check_output(["ls"])
+            print(ls_out.decode("utf-8"))
+
+        elif hypercall == "f":
+            # cat
+            filename = read_str(uc, uc.reg_read(UC_X86_REG_RAX))
+            print("Calling cat on File {}".format(filename))
+            cat_out = subprocess.check_output(["cat", filename])
+            print(cat_out.decode("utf-8"))
+
+        else: 
+            print("Unknown Hypercall!")
+
+    except KeyboardInterrupt as ex:
+        print("\n\nEmulator says baiii <3")
+        uc.emu_stop()
+
+
+def init_emu() -> Uc:
+    # Initialize emulator in X86-64bit mode
+    mu = Uc(UC_ARCH_X86, UC_MODE_64)
+
+    mu.mem_map(0, 16 * 1024 * 1024)
+
+    # write machine code to be emulated to memory
+    mu.mem_write(TXT_POS, GUEST_BINARY)
+
+    mu.reg_write(UC_X86_REG_RSP, 0xFFF000)
+
+    mu.mem_write(0xFFFF00, b"./crux")
+    mu.mem_write(0xFFF000, b"\xFF\xFF\x00")
+
+    # initialize machine registers
+    mu.hook_add(UC_HOOK_INSN, hook_out, None, 1, 0, UC_X86_INS_OUT)
+    return mu
+
+def run_nodebug():
+    mu = init_emu()
+    # emulate code in infinite time & unlimited instructions
+    mu.emu_start(ENTRY, TXT_POS + len(GUEST_BINARY))
+
+def run_debug():
+    """
+    This starts uDdbg.
+    """
+    udbg = UnicornDbg()
+    udbg.initialize(emu_instance=init_emu(),  # We pass in our own unicorn instance with hooks.
+        entry_point=ENTRY,  # Execution will start here
+        exit_point=TXT_POS + len(GUEST_BINARY),  # we never reach this (endless loop)
+        hide_binary_loader=True,  # Our users never want to load other things.
+        mappings=[("main", 0x0, COMPLETE_SIZE)]  # We tell the debugger about the mappings we have.
+    )
+
+    # udbg.add_module(Heapdump(udbg)) # << We can add specific modules here.
+
+    # Kick off emulation.
+    udbg.start()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "run":
+        run_nodebug()
+    else:
+        # uDdbg it.
+        # Example usage:
+        # ./rtooos_emulator.py
+        # b 0x74
+        # c
+        run_debug()  
--- modules/registers.py
+++ modules/registers.py
@@ -30,6 +30,7 @@
 
 from tabulate import tabulate
 from unicorn import *
+from unicorn.x86_const import *
 
 import utils
 from modules.unicorndbgmodule import AbstractUnicornDbgModule
@@ -91,11 +92,119 @@ class Registers(AbstractUnicornDbgModule):
 
     def registers(self, func_name, *args):
         print(utils.titlify('registers'))
-        arch = self.core_instance.unicorndbg_instance.get_arch()
+        arch = self.core_instance.unicorndbg_instance.arch
+        mode = self.core_instance.unicorndbg_instance.mode
         if arch == UC_ARCH_ARM:
             self.print_arm_registers()
+        if arch == UC_ARCH_X86:
+            if mode == UC_MODE_16:
+                self.print_x86_16_registers()
+            elif mode == UC_MODE_32:
+                self.print_x86_registers()
+            else:
+                # If mode is none of the above, let's assume x64.
+                self.print_x64_registers()
         else:
             print('quick registers view: arch not supported')
+
+    @property
+    def emu_instance(self):
+        return self.core_instance.get_emu_instance()
+
+    def reg(self, name: str, uc_const: int):
+        """
+        Create an entry for a new reg, reading the contents from unicorn.
+        """
+        uc = self.emu_instance
+        val = uc.reg_read(uc_const)
+        return [utils.green_bold(name), hex(val), val]
+         
+    def print_x86_16registers(self):
+        r = [
+            self.reg("ip", UC_X86_REG_IP),
+            self.reg("di", UC_X86_REG_DI),
+            self.reg("si", UC_X86_REG_SI),
+            self.reg("ax", UC_X86_REG_AX),
+            self.reg("bx", UC_X86_REG_BX),
+            self.reg("cx", UC_X86_REG_CX),
+            self.reg("dx", UC_X86_REG_DX),
+            self.reg("sp", UC_X86_REG_SP),
+            self.reg("bp", UC_X86_REG_BP),
+            self.reg("eflags", UC_X86_REG_EFLAGS),
+            self.reg("cs", UC_X86_REG_CS),
+            self.reg("gs", UC_X86_REG_GS),
+            self.reg("fs", UC_X86_REG_FS),
+            self.reg("ss", UC_X86_REG_SS),
+            self.reg("ds", UC_X86_REG_DS),
+            self.reg("es", UC_X86_REG_ES)
+        ]
+
+        h = [utils.white_bold_underline('register'),
+             utils.white_bold_underline('hex'),
+             utils.white_bold_underline('decimal')]
+        print(tabulate(r, h, tablefmt="simple"))
+
+
+
+    def print_x86_registers(self):
+        r = [
+            self.reg("eip", UC_X86_REG_EIP),
+            self.reg("edi", UC_X86_REG_EDI),
+            self.reg("esi", UC_X86_REG_ESI),
+            self.reg("eax", UC_X86_REG_EAX),
+            self.reg("ebx", UC_X86_REG_EBX),
+            self.reg("ecx", UC_X86_REG_ECX),
+            self.reg("edx", UC_X86_REG_EDX),
+            self.reg("esp", UC_X86_REG_ESP),
+            self.reg("ebp", UC_X86_REG_EBP),
+            self.reg("eflags", UC_X86_REG_EFLAGS),
+            self.reg("cs", UC_X86_REG_CS),
+            self.reg("gs", UC_X86_REG_GS),
+            self.reg("fs", UC_X86_REG_FS),
+            self.reg("ss", UC_X86_REG_SS),
+            self.reg("ds", UC_X86_REG_DS),
+            self.reg("es", UC_X86_REG_ES)
+        ]
+
+        h = [utils.white_bold_underline('register'),
+             utils.white_bold_underline('hex'),
+             utils.white_bold_underline('decimal')]
+        print(tabulate(r, h, tablefmt="simple"))
+
+
+    def print_x64_registers(self):
+        GSMSR = 0xC0000101
+        FSMSR = 0xC0000100
+        # TODO: Find a way to read FS and GSBASE without clobbering memory.
+        #return get_msr(uc, GSMSR)
+        #return get_msr(uc, FSMSR)
+
+        r = [
+            self.reg("rip", UC_X86_REG_RIP),
+            self.reg("rdi", UC_X86_REG_RDI),
+            self.reg("rsi", UC_X86_REG_RSI),
+            self.reg("rax", UC_X86_REG_RAX),
+            self.reg("rbx", UC_X86_REG_RBX),
+            self.reg("rcx", UC_X86_REG_RCX),
+            self.reg("rdx", UC_X86_REG_RDX),
+            self.reg("rsp", UC_X86_REG_RSP),
+            self.reg("rbp", UC_X86_REG_RBP),
+            self.reg("r8", UC_X86_REG_R8),
+            self.reg("r9", UC_X86_REG_R9),
+            self.reg("r10", UC_X86_REG_R10),
+            self.reg("r11", UC_X86_REG_R11),
+            self.reg("r12", UC_X86_REG_R12),
+            self.reg("r13", UC_X86_REG_R13),
+            self.reg("r14", UC_X86_REG_R14),
+            self.reg("r15", UC_X86_REG_R15),
+            self.reg("eflags", UC_X86_REG_EFLAGS),
+        ]
+
+        h = [utils.white_bold_underline('register'),
+             utils.white_bold_underline('hex'),
+             utils.white_bold_underline('decimal')]
+        print(tabulate(r, h, tablefmt="simple"))
+
 
     def print_arm_registers(self):
         r0 = self.core_instance.get_emu_instance() \
